624eaa3499e7d436d3216f34def96f966341ab18
ShivanshRakesh <49039969+ShivanshRakesh@users.noreply.github.com>
2019-08-08 00:11:01 +0530

    Added notification on update

--- bg_script.js
+++ bg_script.js
@@ -1,13 +1,13 @@
 chrome.tabs.onUpdated.addListener(
     function (tabId, changeInfo, tab) {
         var placeholderCnt = 0;
-        
+
         // FETCH NEW URL
         var url_with_https = changeInfo.url;
         var url_obj = /^http[s]?:\/\/(.+)/.exec(url_with_https);
 
         if (url_obj) {
-            
+
             // DIVIDE THE URL INTO PARTS TO CHECK FOR ANY PLACEHOLDERS
             var url_divs = url_obj[1].split('\/');
             if (url_divs != null && url_divs.length > 1) {
@@ -16,19 +16,19 @@ chrome.tabs.onUpdated.addListener(
 
                 // GET ALIAS ENTRIES FROM CHROME STORAGE
                 chrome.storage.sync.get('entryList', function (result) {
-                
+
                     // PROCEED IF AN ALIAS MATCHES THE FIRST DIVISION OF THE URL
                     if (result.entryList[alias] != null && result.entryList[alias] != undefined) {
                         let urlToRedir = '';
-                
+
                         // GET THE URL REGISTERED WITH THE ALIAS
                         url_stored = result.entryList[alias];
-                
+
                         // SPLIT THE REGISTERED URL TO CHECK FOR ANY PLACEHOLDERS (%%)
                         var splits = url_stored.split('/');
                         for (i = 0; i < splits.length; i++) {
                             var toJoin = '';
-                
+
                             // IF THE PART IS A PLACEHOLDER, REPLACE IT WITH RESPECTIVE DIVISION OF URL ENTERED
                             if (splits[i] == "%%") {
                                 placeholderCnt++;
@@ -58,3 +58,10 @@ chrome.tabs.onUpdated.addListener(
         }
     }
 );
+
+// NOTIFY ON UPDATE
+chrome.runtime.onInstalled.addListener(function (details) {
+    if (details.reason == "update") {
+        alert("URL Aliaser got updated to latest version!");
+    }
+});
